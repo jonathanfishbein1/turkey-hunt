a246af00b52e4c289266a0f72f5d1cdb82d4a5a4
= <=>
2024-07-05 21:56:33 +0000

    build-main-desktop

---
+++ build/main-desktop/js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * This will be loaded before starting the simulator.
+ * If you wish to add custom javascript, 
+ * ** make sure to add this line to pxt.json**
+ * 
+ *      "disableTargetTemplateFiles": true
+ * 
+ * otherwise MakeCode will override your changes.
+ * 
+ * To register a constrol simmessages, use addSimMessageHandler
+ */
+addSimMessageHandler("web", (data) => {
+    console.log('addSimMessageHandler')
+    const targetWindow = window.parent
+    console.log('window ', window)
+    console.log('targetWindow ', targetWindow)
+    targetWindow.postMessage(data, 'http://localhost:8080')
+})
